5fb31891e8e44952176b51a840b58885f383ff74
Javier de Pedro López <javier.pedro@mobgen.com>
2016-07-04 15:14:54 +0200

    Setup the repository

---
+++ app/src/main/java/com/mobgen/droidcon/offline/DemoSelectorActivity.java
@@ -0,0 +1,13 @@
+package com.mobgen.droidcon.offline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_selector);
+    }
+}
